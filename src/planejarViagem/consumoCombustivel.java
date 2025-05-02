cc7794f32003bfc668adf1ec896bd6797461519f
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-02 19:59:01 -0300

    feat: calcular consumo de combustivel

---
+++ src/planejarViagem/consumoCombustivel.java
@@ -0,0 +1,23 @@
+package planejarViagem;
+
+public class consumoCombustivel {
+    public static void main(String[] args) {
+
+        double consumoMedio = 12.5; // O carro faz 12,5 km por litro
+        double capacidadeTanque = 50; // O tanque tem capacidade para 50 litros
+        double combustivelAtual = 20; // Há 20 litros disponíveis
+        double distanciaViagem = 200; // A viagem planejada tem 200 km
+
+        double autonomiaMax = consumoMedio * capacidadeTanque; //quantos quilômetros pode percorrer com um tanque cheio
+        double autonomiaAtual = consumoMedio * combustivelAtual;
+
+        System.out.println("Autonomia máxima do veículo: " + autonomiaMax + "km");
+        System.out.println("Autonomia atual: " + autonomiaAtual + "km");
+
+        if(autonomiaAtual >= distanciaViagem) {
+            System.out.println("Você conseguirá completar a viagem sem precisar abastecer.");
+        } else {
+            System.out.println("Você não conseguirá completar a viagem sem precisar abastecer.");
+        }
+    }
+}
