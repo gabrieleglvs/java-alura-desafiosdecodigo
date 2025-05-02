504fa74ba0bac921db2bc643ae7c3fa158d93528
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-02 16:16:08 -0300

    feat: converter tipo double para int

---
+++ src/conversaoDeTipos/converterTipo.java
@@ -0,0 +1,11 @@
+package conversaoDeTipos;
+
+public class converterTipo {
+
+    //Para atender às exigências do sistema, crie um programa que converta a quantidade de double para int, garantindo que os valores sejam adequados ao formato aceito.
+    public static void main(String[] args) {
+        double valorDouble = 19.5;
+
+        System.out.println("O valor inteiro do produto é: " + (int) valorDouble);
+    }
+}
--- src/variaveisETipos/declararVariaveis.java
+++ src/variaveisETipos/declararVariaveis.java
@@ -1,2 +1,18 @@
-package variaveisETipos;public class declararVariaveis {
+package variaveisETipos;
+
+public class declararVariaveis {
+
+    //Escreva um programa que declare variáveis para o seu nome, idade, altura e se você é estudante, utilizando os tipos apropriados, e depois exiba os valores armazenados.
+
+    public static void main(String[] args) {
+        String nome = "Gabriele";
+        int idade = 25;
+        float altura = 1.59f;
+        boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudante: " + estudante);
+    }
 }
