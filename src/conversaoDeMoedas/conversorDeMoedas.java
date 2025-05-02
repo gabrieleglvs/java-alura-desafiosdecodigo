a6afcf0330c12c4fd5871da85bfcdc0f74578eb1
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-02 19:41:16 -0300

    feat: converter real para dolar

---
+++ src/conversaoDeMoedas/conversorDeMoedas.java
@@ -0,0 +1,10 @@
+package conversaoDeMoedas;
+
+public class conversorDeMoedas {
+    public static void main(String[] args) {
+        double valorReais = 451.50;
+        double conversaoDolar = valorReais / 5.25;
+
+        System.out.println("O valor em dólares é: US$ " + conversaoDolar);
+    }
+}
