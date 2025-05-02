ad7296923f62456298234e1ddcb7f36c39366a5b
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-02 19:29:49 -0300

    feat: verificar se o numero é impar ou par

--- src/parOuImpar/parOuImpar.java
+++ src/parOuImpar/parOuImpar.java
@@ -1,2 +1,13 @@
-package parOuImpar;public class parOuImpar {
+package parOuImpar;
+
+public class parOuImpar {
+    public static void main(String[] args) {
+        int numero = 6;
+
+        if(numero % 2 == 0) {
+            System.out.println("O número é par");
+        } else {
+            System.out.println("O número é ímpar");
+        }
+    }
 }
